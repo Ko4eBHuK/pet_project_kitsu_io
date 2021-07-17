0a97559a2bc5fe73eeaf9adff90c2f9ef383e28c
Ko4eBHuK <okladnikov.bool@gmail.com>
2021-07-18 01:10:53 +0800

    initial commit

---
+++ android/app/src/main/kotlin/okladnikov/bool/pet_project_kitsu_io/MainActivity.kt
@@ -0,0 +1,6 @@
+package okladnikov.bool.pet_project_kitsu_io
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KITSU.IO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'KITSU.IO APP home page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
